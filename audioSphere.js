173eefffb7c74de884a5e1f9db89ad54852a1f6b
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2018-12-22 15:07:03 -0500

    Big refactor: Now works with newer Chrome
    The audio had to be started only after a user event occured.
    This commit also removes a lot of extraneous code.

--- audioSphere.js
+++ audioSphere.js
@@ -1,80 +1,39 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var stats = new Stats();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const stats = new Stats();
 
-var renderer = new THREE.WebGLRenderer({
+const renderer = new THREE.WebGLRenderer({
     antialias: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
-//renderer.setClearColor(0xFF0000);
+// renderer.setClearColor(0xFF0000);
 renderer.sortObjects = true;
 document.body.appendChild(renderer.domElement);
 document.body.appendChild(stats.domElement);
-camera.position.z = 200;
+camera.position.z = 270;
+camera.position.y = 100;
 
-var orbit = new THREE.OrbitControls(camera, render.domElement);
-
-var light = new THREE.AmbientLight(0x000000);
-scene.add(light);
-
-var lights = [];
-lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-lights[2] = new THREE.PointLight(0xffffff, 1, 0);
-
-lights[0].position.set(0, 200, 0);
-lights[1].position.set(100, 200, 100);
-lights[2].position.set(-100, -200, -100);
-
-scene.add(lights[0]);
-scene.add(lights[1]);
-scene.add(lights[2]);
-
-//Create the 3D text
-var mesh = new THREE.Object3D();
-var geometry = new THREE.TextGeometry("Jerome", {
-    size: 5,
-    height: 2,
-    curveSegments: 24,
-    font: "helvetiker",
-    weight: "normal",
-    style: "normal",
-    bevelEnabled: false,
-    bevelThickness: 1,
-    bevelSize: 0.5
-});
-
-geometry.center();
-
-mesh.add(new THREE.Mesh(
-    geometry,
-    new THREE.MeshLambertMaterial({
-        color: 0x156289
-    })
-));
-
-mesh.renderOrder = 0;
-scene.add(mesh);
+const orbit = new THREE.OrbitControls(camera, render.domElement);
 
 /* PARTICLE SYSTEM */
-var radius = 100;
-var num_orbits = 40;
-var arc_separation = 1;
-var particles = new THREE.Geometry();
-var particleMaterial = new THREE.PointsMaterial({
-    color: 0xFFFFFF,
+const radius = 100;
+const num_orbits = 40;
+const arc_separation = 1;
+const particles = new THREE.Geometry();
+const particleMaterial = new THREE.PointsMaterial({
+    color: 0xffffff,
     size: 4,
-    map: new THREE.TextureLoader().load("res/particle.png"),
+    map: new THREE.TextureLoader().load('res/particle.png'),
     blending: THREE.AdditiveBlending,
     transparent: true
 });
 
-for(var theta = -Math.PI * (1/2 - 1/num_orbits); theta < Math.PI / 2; theta += Math.PI / num_orbits) {
-    var orbitRadius = Math.cos(theta) * radius;
-    var particleCount = orbitRadius / arc_separation; //Find the number of particles to put on that orbit
-    for(var phi = 0; phi < 2 * Math.PI; phi += 2 * Math.PI / particleCount) {
-        var particle = new THREE.Vector3(0, 0, 0);
-        particle.x = particle.initX = Math.cos(theta) * Math.cos(phi) *  radius;
+for (let theta = -Math.PI * (1 / 2 - 1 / num_orbits); theta < Math.PI / 2; theta += Math.PI / num_orbits) {
+    const orbitRadius = Math.cos(theta) * radius;
+    const particleCount = orbitRadius / arc_separation; // Find the number of particles to put on that orbit
+    for (let phi = 0; phi < 2 * Math.PI; phi += (2 * Math.PI) / particleCount) {
+        const particle = new THREE.Vector3(0, 0, 0);
+        particle.x = particle.initX = Math.cos(theta) * Math.cos(phi) * radius;
         particle.y = particle.initY = Math.sin(theta) * radius;
         particle.z = particle.initZ = Math.sin(phi) * orbitRadius;
 
@@ -82,50 +41,60 @@ for(var theta = -Math.PI * (1/2 - 1/num_orbits); theta < Math.PI / 2; theta += M
     }
 }
 
-//Create the particle system
-var particleSystem = new THREE.Points(particles, particleMaterial);
+// Create the particle system
+const particleSystem = new THREE.Points(particles, particleMaterial);
 scene.add(particleSystem);
 
-/*var grid = new THREE.GridHelper(100, 10);
-scene.add(grid);*/
+/* AUDIO - Needs a user event to start */
+let analyser;
+let frequencyData;
+const skipFrequencies = 620; // Skip the first frequencies as they have too big values and mess up the shape of the sphere
+const playBtn = document.getElementById('play');
+playBtn.addEventListener('click', () => {
+    const audioCtx = new AudioContext();
+    analyser = audioCtx.createAnalyser();
+    const audioEl = document.getElementById('audio');
+    const source = audioCtx.createMediaElementSource(audioEl);
+    source.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    analyser.fftSize = 8192;
+    analyser.smoothingTimeConstant = 0.8;
 
+    frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-/* AUDIO */
-var audioCtx = new AudioContext();
-var analyser = audioCtx.createAnalyser();
-var source = audioCtx.createMediaElementSource(audio);
-var skipFrequencies = 620; //Skip the first frequencies as they have too big values and mess up the shape of the sphere
-source.connect(analyser);
-analyser.connect(audioCtx.destination);
-analyser.fftSize = 8192;
-analyser.smoothingTimeConstant = 0.8;
+    audioEl.play();
 
-var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-audio.pause();
-audio.play();
+    // Hide the play button
+    playBtn.setAttribute('style', 'display: None;');
+
+    // Show the audio controls now
+    audioEl.setAttribute('style', 'display: block;');
+});
 
 function render() {
     requestAnimationFrame(render);
 
-    //Update the particles
-    analyser.getByteFrequencyData(frequencyData);
+    // Update the particles
+    if (frequencyData) {
+        analyser.getByteFrequencyData(frequencyData);
 
-    for (var i = 0; i < particles.vertices.length / 2; i++) {
-        if (i + skipFrequencies < frequencyData.length) {
-            var particle = particles.vertices[Math.floor(particles.vertices.length / 2) + i];
-            var factor = frequencyData[i + skipFrequencies] / 256 + 1; //between 1 and 2
-            particle.x = particle.initX * factor;
-            particle.y = particle.initY * factor;
-            particle.z = particle.initZ * factor;
+        for (let i = 0; i < particles.vertices.length / 2; i++) {
+            if (i + skipFrequencies < frequencyData.length) {
+                let particle = particles.vertices[Math.floor(particles.vertices.length / 2) + i];
+                const factor = frequencyData[i + skipFrequencies] / 256 + 1; // between 1 and 2
+                particle.x = particle.initX * factor;
+                particle.y = particle.initY * factor;
+                particle.z = particle.initZ * factor;
 
-            particle = particles.vertices[Math.floor(particles.vertices.length / 2) - i];
-            particle.x = particle.initX * factor;
-            particle.y = particle.initY * factor;
-            particle.z = particle.initZ * factor;
+                particle = particles.vertices[Math.floor(particles.vertices.length / 2) - i];
+                particle.x = particle.initX * factor;
+                particle.y = particle.initY * factor;
+                particle.z = particle.initZ * factor;
+            }
         }
-    }
 
-    particleSystem.geometry.verticesNeedUpdate = true; //Need to specify that the object has changed
+        particleSystem.geometry.verticesNeedUpdate = true; // Need to specify that the object has changed
+    }
 
     orbit.update();
     stats.update();
@@ -134,11 +103,13 @@ function render() {
 }
 render();
 
-window.addEventListener('resize', function () {
+window.addEventListener(
+    'resize',
+    () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-}, false);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    },
+    false
+);
